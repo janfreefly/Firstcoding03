927fd67d4fa1619e852ebf462138b5acbb862f87
Mac <Mac@Mac-PC>
2017-03-07 12:57:52 +0800

    2017030501

---
+++ 1/src/tw/org/iii/Hello.java
@@ -0,0 +1,8 @@
+package tw.org.iii;
+
+public class Hello {
+	public static void main(String[] jeff){
+		System.out.println("Hello, World");
+		System.out.println("Hello, Jeff");
+	}
+}
---
+++ 1/src/tw/org/iii/jeff01.java
@@ -0,0 +1,16 @@
+package tw.org.iii;
+
+public class jeff01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/**
+		*
+		* @parma jeff
+		*/
+		
+
+	}
+
+}
