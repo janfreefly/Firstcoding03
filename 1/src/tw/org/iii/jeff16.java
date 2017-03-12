b4e99e55ae1150a90671f08fc9bac0757d718fa0
Mac <Mac@Mac-PC>
2017-03-12 16:17:46 +0800

    3/12

--- 1/src/tw/org/iii/jeff16.java
+++ 1/src/tw/org/iii/jeff16.java
@@ -1,20 +1,67 @@
 package tw.org.iii;
 
-public class jeff16 {
+import javax.swing.JOptionPane;
 
+/*
+ * Program: Guess Number game
+ */
+public class Brad17 {
+	
 	public static void main(String[] args) {
-		int a = 1; int sum = 0;
-		while(a<100){
-			sum += a++;
-		}
-		System.out.println(sum);
-		System.out.println("-----");
+		// 1. Create Answer
+		String answer = createAnswer(3);
+		System.out.println(answer);
 		
-		a = 1; sum = 0;
-		for(;a<100;){
-			sum += a++;
+		// 2. Start
+		for (int i=0; i<10; i++){
+			// 2-1. input a number string
+			String guess = JOptionPane.showInputDialog("猜數字遊戲");
+			
+			// 2-2. compare string => result
+			String result = checkAB(answer, guess); 
+			JOptionPane.showMessageDialog(null, result);
+			// 2-3. WINNER -> break
+			// 2-4. repeat 10 times > LOSSER
 		}
-		System.out.println(sum);
-		System.out.println("-----");
+		
+		// 3. show ..... / .... 
 	}
+	
+	// method: checkAB(answer, guess) => ?A?B 
+	static String checkAB(String a, String g){
+		int A, B; A = B = 0;
+		for (int i=0; i<g.length(); i++){
+			
+		}
+		
+		return "1A2B";
+	}
+	
+	
+	// method: create a answer
+	static String createAnswer(int d){
+		// 瘣��
+		boolean[] check = new boolean[10];	// 0 - 9: false
+		int[] poker = new int[d]; // 0-2: 0
+		int temp; int counter=0;
+		for (int i=0; i<poker.length; i++){
+			do{
+				temp = (int)(Math.random()*10);
+				counter++;
+			}while (check[temp]);
+			poker[i] = temp;
+			check[temp] = true;
+		}
+		String ret = "";
+		for(int v:poker) ret += v;
+		
+		return ret;
+	}
+
+	
+	
+	
+	
+	
+	
 }
---
+++ 1/src/tw/org/iii/jeff19.java
@@ -0,0 +1,13 @@
+package tw.org.iii;
+
+public class jeff19 {
+
+	public static void main(String[] args) {
+		String a = new String("abc");
+		String b = new String("abc");
+		String c = "abc";
+		String d = "abc";
+		System.out.println(a == b);
+	}
+
+}
